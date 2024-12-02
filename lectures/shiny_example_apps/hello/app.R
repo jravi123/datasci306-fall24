dd3f4e55bfef29e3e3146cc35aae649a9d241f34
jravi123 <jayaravi@stat-jayaravi-mbp.local>
2024-12-02 13:56:36 -0500

    added content

---
+++ lectures/shiny_example_apps/hello/app.R
@@ -0,0 +1,23 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  output$greeting <- renderText({
+    paste0("Hello ", input$name, "!")
+  })
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
